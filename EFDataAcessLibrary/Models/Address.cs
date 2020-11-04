912c5bc41dfd140cade4fa7f3d39b36b33fc656a
David Trigueiros <david.ordaz96@gmail.com>
2020-11-04 11:55:53 +0000

    Add project files.

---
+++ EFDataAcessLibrary/Models/Address.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EFDataAcessLibrary.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string StreetAdress { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string City { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string State { get; set; }
+
+        [Required]
+        [MaxLength(10)] // 12345-0000
+        [Column(TypeName = "varchar(10)")]
+        public string ZipCode { get; set; } // 54215-1234
+
+    }
+}
---
+++ EFDemoWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using EFDataAcessLibrary.DataAccess;
+using EFDataAcessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EFDemoWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly PeopleContext _db;
+
+        public IndexModel(ILogger<IndexModel> logger, PeopleContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            LoadSampleDate();
+
+            var people = _db.People
+                .Include(a => a.Addresses)
+                .Include(e => e.EmailAddresses)
+                //.Where(x => ApprovedAge(x.Age)); Not Efficient, will get all records, and only then filter them
+                .Where(x => x.Age >= 18 && x.Age <= 65)
+                .ToList();
+        }
+
+        private bool ApprovedAge(int age)
+        {
+            return (age >= 18 && age <= 65);
+        }
+
+        private void LoadSampleDate()
+        {
+            if(_db.People.Count() == 0)
+            {
+                string file = System.IO.File.ReadAllText("generated.json");
+                var people = JsonSerializer.Deserialize<List<Person>>(file);
+                _db.AddRange(people);
+                _db.SaveChanges();
+            }
+        }
+    }
+}
